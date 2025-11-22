3296a66c2cef173e1eb131c17dc72c96615f08a2
Raj Nandigalla <nraj09@yahoo.com>
2025-11-22 21:04:29 +0530

    chore: Remove `webDir` and update `server.url` in Capacitor configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.leafink.app',
   appName: 'LeafInk',
-  webDir: 'out',
+  // webDir: 'out', NOTE: For Static App
   server: {
-    url: 'http://localhost:3000',
+    url: 'http://192.168.1.110:3000',
     cleartext: true,
   },
 };
